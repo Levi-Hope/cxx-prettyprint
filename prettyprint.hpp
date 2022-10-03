4b96ff46ad8afcf3373764586a5996f69aec1164
levi <2421860260@qq.com>
2022-10-03 17:17:28 +0800

    feat: add colourful output

--- prettyprint.hpp
+++ prettyprint.hpp
@@ -490,6 +490,27 @@ namespace std
 // reference1: https://stackoverflow.com/questions/6622962/generic-way-to-print-out-variable-name-in-c
 // reference2: https://stackoverflow.com/questions/11761703/overloading-macro-on-number-of-arguments
 
-
+/*
+[Colourful Output]
+Example:
+cout << RED << "hello world" << RESET << endl;
+*/
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 #endif  // H_PRETTY_PRINT
